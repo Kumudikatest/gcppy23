5eff93f835e29ae8c6aba0f91eb74721048bff6c
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2021-03-23 17:07:47 +0530

    add cog

--- gcppy23.py
+++ gcppy23.py
@@ -1,3 +1,15 @@
+import boto3
+cognito_idp = boto3.client("cognito-idp")
+
 def handler(request):
+    try:
+        data = cognito_idp.list_users(
+            UserPoolId="us-east-1_HdYJb7Znp",
+            Limit=10
+        )
+        print (data)
+    except BaseException as e:
+        print(e)
+        raise(e)
     
     return "Successfully executed"
